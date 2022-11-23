46d91fdb065bc32f64818f0f2212d046bdf0c53e
Hanekawa-san <captainduck.official@gmail.com>
2022-11-24 01:21:15 +0300

    maybe final?

--- src/screens/IntMap.js
+++ src/screens/IntMap.js
@@ -1,7 +1,7 @@
 import GoBackButton from "../components/GoBackButton";
-import {useEffect, useState} from "react";
+import {useEffect, useState, cloneElement} from "react";
 import {Radar} from "react-chartjs-2"
-import { Graph, DefaultLink, DefaultNode } from '@visx/network';
+import Diagram, { createSchema, useSchema } from 'beautiful-react-diagrams';
 import {
     Chart as ChartJS,
     RadialLinearScale,
@@ -11,6 +11,7 @@ import {
     Tooltip,
     Legend,
 } from 'chart.js';
+import LoadingScreen from "../components/LoadingScreen";
 
 const IntMap = () => {
     ChartJS.register(
@@ -22,13 +23,20 @@ const IntMap = () => {
         Legend
     );
     const [results, setResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        if(results.length > 0) {
+            setIsLoading(false);
+        }
+    }, [results]);
 
     useEffect(() => {
         let res = [];
         for (let i = 1; i < 4; i++) {
-            let resultPol = (parseInt(localStorage.getItem('result_'+i+'_1')));
-            let resultChl = (parseInt(localStorage.getItem('result_'+i+'_2')));
-            let resultUmn = (parseInt(localStorage.getItem('result_'+i+'_3')));
+            const resultPol = (parseInt(localStorage.getItem('result_'+i+'_1')));
+            const resultChl = (parseInt(localStorage.getItem('result_'+i+'_2')));
+            const resultUmn = (parseInt(localStorage.getItem('result_'+i+'_3')));
             const data = {
                 labels: ['POL', 'CHL', 'UMN'],
                 datasets: [
@@ -43,42 +51,153 @@ const IntMap = () => {
             };
             res.push(data);
         }
+        let data = {
+            labels: ['POL', 'CHL', 'UMN'],
+            datasets: [
+                {
+                    label: 'баллы',
+                    data: [(res[0].datasets[0].data[0] + res[1].datasets[0].data[0])/2,
+                        (res[0].datasets[0].data[1] + res[1].datasets[0].data[1])/2,
+                        (res[0].datasets[0].data[2] + res[1].datasets[0].data[2])/2],
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1,
+                },
+            ],
+        };
+        res.push(data)
+        data = {
+            labels: ['POL', 'CHL', 'UMN'],
+            datasets: [
+                {
+                    label: 'баллы',
+                    data: [(res[2].datasets[0].data[0] + res[3].datasets[0].data[0])/2,
+                        (res[2].datasets[0].data[1] + res[3].datasets[0].data[1])/2,
+                        (res[2].datasets[0].data[2] + res[3].datasets[0].data[2])/2],
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1,
+                },
+            ],
+        };
+        res.push(data)
         setResults(res);
     },[]);
 
-    const nodes = [
-        { x: 50, y: 20 },
-        { x: 200, y: 300 },
-        { x: 300, y: 40 },
-    ];
+    const Node = (props) => {
+        const { inputs, outputs, data } = props;
 
-    const dataSample = {
-        nodes,
-        links: [
-            { source: nodes[0], target: nodes[1] },
-            { source: nodes[1], target: nodes[2] },
-            { source: nodes[2], target: nodes[0] },
-        ],
+        return (
+            <div className="max-h-64">
+                <Radar
+                    width={1}
+                    height={1}
+                    data={results[data.themeId]}
+                />
+                <div style={{marginTop: '20px'}}>
+                    {inputs.map((port) => cloneElement(port, {
+                        style: { width: '50px', height: '25px', background: '#1B263B' }
+                    }))}
+                    {outputs.map((port) => cloneElement(port, {
+                        style: { width: '50px', height: '25px', background: '#1B263B' }
+                    }))}
+                </div>
+            </div>
+        );
     };
 
-    const MyGraph = () => (
-        <Graph graph={dataSample} linkComponent={DefaultLink} nodeComponent={DefaultNode} />
-    );
+    // const initialSchema = createSchema({
+    //     nodes: [
+    //         {
+    //             id: 'node1',
+    //             coordinates: [150, 60],
+    //             render: Node,
+    //             disableDrag: true,
+    //             inputs: [ { id: 'port-1', alignment: 'right' } ],
+    //             data: {
+    //                 themeId: 0
+    //             }
+    //         },
+    //         {
+    //             id: 'node2',
+    //             coordinates: [250, 60],
+    //             render: Node,
+    //             disableDrag: true,
+    //             outputs: [ { id: 'port-2',  alignment: 'left' } ],
+    //             data: {
+    //                 themeId: 1
+    //             }
+    //         },
+    //     ],
+    //     links: [
+    //         { input: 'port-1',  output: 'port-2', readonly: true, label: "yo" },
+    //     ]
+    // });
+
+    // const UncontrolledDiagram = () => {
+    //     const [schema, { onChange }] = useSchema(initialSchema);
+    //
+    //     return (
+    //         <div style={{ height: '22.5rem' }}>
+    //             <Diagram schema={schema} onChange={onChange} />
+    //         </div>
+    //     );
+    // };
 
     return (
+        <>
+            {isLoading ? (<LoadingScreen/>) : (
         <section className="bg-gray-50">
             <GoBackButton/>
-            <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+            <div className="mx-auto max-w-screen-xl px-4 lg:flex lg:h-screen lg:items-center">
                 <div className="mx-auto max-w-xl text-center">
                     <h1 className="text-3xl font-extrabold sm:text-5xl">
                         Интеллектуальная карта
                     </h1>
-                    <Radar
-                        data={results[0]}
-                    />
+                    {/*<UncontrolledDiagram />*/}
+                    <div className="flex flex-col">
+                        <div className="flex flex-row">
+                            <div className="max-h-128">
+                                Результаты 1 теста
+                                <Radar
+                                    data={results[0]}
+                                />
+                            </div>
+                            <div className="max-h-128">
+                                Результаты 2 теста
+                                <Radar
+                                    data={results[1]}
+                                />
+                            </div>
+                            <div className="max-h-128">
+                                Результаты 3 теста
+                                <Radar
+                                    data={results[2]}
+                                />
+                            </div>
+                        </div>
+                        <div className="flex flex-row">
+                            <div className="max-h-128">
+                                Результаты 1+2 теста
+                                <Radar
+                                    data={results[3]}
+                                />
+                            </div>
+                        </div>
+                        <div className="flex flex-row">
+                            <div className="max-h-128">
+                                Итоговые результаты
+                                <Radar
+                                    data={results[4]}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </section>
+        </section>)
+            }
+        </>
     )
 }
 
--- src/screens/Result.js
+++ src/screens/Result.js
@@ -1,43 +1,58 @@
 import {useEffect, useState} from "react";
-import {ArrowLeftIcon} from "@heroicons/react/20/solid";
 import {useNavigate, useParams} from "react-router-dom";
+import {Radar} from "react-chartjs-2";
+import GoBackButton from "../components/GoBackButton";
+import {Chart as ChartJS, Filler, Legend, LineElement, PointElement, RadialLinearScale, Tooltip} from "chart.js";
+import LoadingScreen from "../components/LoadingScreen";
 
 const Result = () => {
-    const [resultChl, setResultChl] = useState('');
-    const [resultPol, setResultPol] = useState('');
-    const [resultUmn, setResultUmn] = useState('');
-    const navigate = useNavigate();
+    ChartJS.register(
+        RadialLinearScale,
+        PointElement,
+        LineElement,
+        Filler,
+        Tooltip,
+        Legend
+    );
+    const [resultChl, setResultChl] = useState(0);
+    const [resultPol, setResultPol] = useState(0);
+    const [resultUmn, setResultUmn] = useState(0);
     const params = useParams();
     const themeId = params.themeId;
 
-    const getBack = () => {
-        navigate("/");
-    }
-
     useEffect(() => {
-        setResultPol(localStorage.getItem('result_'+themeId+'_1'));
-        setResultChl(localStorage.getItem('result_'+themeId+'_2'));
-        setResultUmn(localStorage.getItem('result_'+themeId+'_3'));
+        setResultPol(parseInt(localStorage.getItem('result_'+themeId+'_1')));
+        setResultChl(parseInt(localStorage.getItem('result_'+themeId+'_2')));
+        setResultUmn(parseInt(localStorage.getItem('result_'+themeId+'_3')));
     },[]);
+
+    const data = {
+        labels: ['POL', 'CHL', 'UMN'],
+        datasets: [
+            {
+                label: 'баллы',
+                data: [resultPol, resultChl, resultUmn],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1,
+            },
+        ],
+    };
 
     return(
         <>
             <div className="bg-gray-50">
-                <ArrowLeftIcon className="max-h-8 max-w-8 cursor-pointer hover:bg-gray-200" onClick={getBack}/>
+                <GoBackButton/>
             </div>
             <section className="bg-gray-50">
                 <div className="mx-auto max-w-screen-4xl px-4 py-24 lg:flex lg:h-screen lg:items-center">
-
                     <div className="mx-auto max-w-4xl text-center">
-                        <h1 className="text-2xl font-bold sm:text-5xl">
-                            Результат теста на полноту знаний: {resultPol}
+                        <h1 className="text-3xl font-extrabold sm:text-5xl">
+                            Результаты теста №{themeId}
                         </h1>
-                        <h1 className="text-2xl font-bold sm:text-5xl">
-                            Результат теста на целостность знаний: {resultChl}
-                        </h1>
-                        <h1 className="text-2xl font-bold sm:text-5xl">
-                            Результат теста на умения: {resultUmn}
-                        </h1>
+                        <Radar
+                            data={data}
+                        />
                     </div>
                 </div>
             </section>
